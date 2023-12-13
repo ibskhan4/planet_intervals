2d2971ea62cb247cd36f26c8ce12516765183161
Ibraheem Khan <ikhan@college.harvard.edu>
2023-12-13 10:56:30 -0600

    Update planets.py

--- planets.py
+++ planets.py
@@ -1,15 +1,4 @@
-# A spaceship is approaching some planets that are arranged in a 1-dimensional line. It needs to pass through the line of the planets, but can't get too close to any planet, otherwise it would get caught in that
-# planet's gravitational field.
-# Given a list of planets, where each planet is specified by its position along the line and the range of its gravitational field, compute the list of gaps from [0, 1000] through which the spaceship can fly.
-
-
-
-# input                output (gaps)           reasoning
-# [location, orbit]
-# [3,2]                [0,1],[5,1000]          The field covers [1,5].
-# [3,2],[5,1]          [0,1],[6,1000]          The fields cover [1,5] and [4,6].
-# [2,1],[5,1]          [0,1],[3,4],[6,1000]    The fields cover [1,3] and [4,6].
-# [2,2],[7,1],[4,1]    [5,6],[8,1000]          The fields cover [0,4], [6,8], and [3,5].
+# Test cases
 
 input = [[2,2],[7,1],[4,1]]
 input2 = [[3,3],[5,1],[990,15]]
@@ -51,34 +40,7 @@ def gaps(planets):
 
     print(f"The gaps in between planets are {res}")
 
-#gaps(input2)
 
-
-
-
-hand = ['10H','QC','4S', 'QS']
-def order(cards):
-    cardVals = { str(i): i for i in range(2,11) }
-    cardVals['J'] = 11
-    cardVals['Q'] = 12
-    cardVals['K'] = 13
-    cardVals['A'] = 14
-
-    suitVals = {
-        'S': 4,
-        'C': 3,
-        'H': 2,
-        'D': 1
-    }
-    val, suit = cards[:-1], cards[-1]
-    print(val, suit)
-    return (cardVals[val], suitVals[suit])
-
-def arrangeHand(hand):
-    hand.sort(key = order, reverse = True)
-    print(hand)
-
-arrangeHand(hand)
 
 
 
